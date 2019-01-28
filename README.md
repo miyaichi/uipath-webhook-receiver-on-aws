7077e218e24032a8ec72cd039ffbdf7cd88a311f
EC2 Default User <miyaichi@gmail.com>
2019-01-28 09:18:09 +0000

    Update README

--- README.md
+++ README.md
@@ -26,6 +26,19 @@ $ serverless deploy [--stage production]
 
 ## Use case
 
+### Ticket Service Integration
+![Use Case #1](https://user-images.githubusercontent.com/129797/51825579-6a43c100-2328-11e9-821f-18784398d09b.png)
+
+### Slack (Slask Command) Integration
+![Use Case #2](https://user-images.githubusercontent.com/129797/51825724-b858c480-2328-11e9-93bc-bd87aeaad2a9.png)
+
+### IoT Enterprise Button Integration
+![Use Case #3](https://user-images.githubusercontent.com/129797/51825742-c1e22c80-2328-11e9-8b3b-4d9a214a0ee9.png)
+
+### HTML Form Integration
+![Use Case #4](https://user-images.githubusercontent.com/129797/51825755-cc9cc180-2328-11e9-812f-f4f5df68cd7a.png)
+
+
 * ticket service
 * html form
 * aws iot button
--- handlers/http.py
+++ handlers/http.py
@@ -2,20 +2,41 @@
 import json
 import os
 import uipath
+from jinja2 import Environment, FileSystemLoader
+from urlparse import parse_qs
 
 
 def handler(event, context):
-    process_name = os.environ["process_name"]
-    if "process_name" in event:
-        process_name = event["process_name"]
+    params = parse_qs(event["body"])
 
-    if (process_name):
-        body = {"message": "process_name not found"}
-        response = {"statusCode": 200, "body": json.dumps(body)}
+    process_name = os.environ["process_name"]
+    if "process_name" in params:
+        process_name = params["process_name"][0]
+
+    env = Environment(loader=FileSystemLoader('./templates', encoding='utf8'))
+    tpl = env.get_template('response.tpl.html')
+
+    if (not process_name):
+        message = "process_name not found"
+        response = {
+            "statusCode": 200,
+            "headers": {
+                'Content-Type': 'text/html;charset=UTF-8'
+            },
+            "body": tpl.render({
+                "message": message
+            })
+        }
         return response
 
-    process_name = event["placementInfo"]["attributes"]["process_name"]
     message = uipath.start_jobs(process_name)
-    body = {"message": message}
-    response = {"statusCode": 200, "body": json.dumps(body)}
+    response = {
+        "statusCode": 200,
+        "headers": {
+            'Content-Type': 'text/html;charset=UTF-8'
+        },
+        "body": tpl.render({
+            "message": message
+        })
+    }
     return response
