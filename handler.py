ebdab2ed28dde71713a969e8978b605bc924add3
EC2 Default User <miyaichi@gmail.com>
2019-01-28 05:49:54 +0000

    Update

--- handler.py
+++ handler.py
@@ -2,42 +2,41 @@
 import json
 import logging
 
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
 
-
-def logging(func):
-    def decorate(*args, **kwds):
-        log.debug("Received event {}".format(json.dumps(args[0])))
-        response = func(*args, **kwds)
+def monitoring(func):
+    def decorate(event, context):
+        log = logging.getLogger()
+        log.setLevel(logging.DEBUG)
+        log.debug("Received event {}".format(json.dumps(event)))
+        response = func(event, context)
         log.debug("Response {}".format(json.dumps(response)))
         return response
 
     return decorate
 
 
-@logging
+@monitoring
 def backlog_handler(event, context):
     import handlers.backlog
     response = handlers.backlog.handler(event, context)
     return response
 
 
-@logging
+@monitoring
 def http_handler(event, context):
     import handlers.http
     response = handlers.http.handler(event, context)
     return response
 
 
-@logging
+@monitoring
 def iot_button_handler(event, context):
     import handlers.iot_button
     response = handlers.iot_button.handler(event, context)
     return response
 
 
-@logging
+@monitoring
 def slash_command_handler(event, context):
     import handlers.slash_command
     response = handlers.slash_command.handler(event, context)
