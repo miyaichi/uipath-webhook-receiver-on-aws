e5f13c275b57cd09cdd140bd14fd2d4bf293df9c
EC2 Default User <miyaichi@gmail.com>
2019-02-04 00:03:06 +0000

    add function find_process_name()

--- uipath.py
+++ uipath.py
@@ -7,12 +7,22 @@ import requests
 
 def start_jobs(process_name):
     o = orchestrator()
-    if o.account_authenticate() and o.find_release(
+    if o.account_authenticate() and o.find_release_key(
             process_name) and o.start_jobs():
         message = _("{} started. Job ID is {}").format(process_name, o.job_id)
     else:
-        message = _("{} can not started. message is {}").format(process_name, o.message)
+        message = _("{} can not started. message is {}").format(
+            process_name, o.message)
     return message
+
+
+def find_process_name(release_key):
+    o = orchestrator()
+    if o.account_authenticate() and o.find_process_name(release_key):
+        process_name = o.process_name
+    else:
+        process_name = None
+    return process_name
 
 
 class orchestrator:
@@ -24,6 +34,7 @@ class orchestrator:
         self.api_key = os.environ["orchestrator_api_key"]
         self.queue_name = os.environ["orchestrator_queue_name"]
         self.token = None
+        self.process_name = None
         self.release_key = None
         self.job_id = None
         self.message = None
@@ -49,7 +60,7 @@ class orchestrator:
             self.message = _("{} failed").format("/api/Account/Authenticate")
         return self.token
 
-    def find_release(self, process_name):
+    def find_release_key(self, process_name):
         if self.token is None:
             return None
 
@@ -68,7 +79,31 @@ class orchestrator:
             if response.json()["@odata.count"] > 0:
                 self.release_key = response.json()["value"][0]["Key"]
             else:
-                self.message = _("relasse not found")
+                self.message = _("Relasse not found")
+        else:
+            self.message = _("{} failed").format("/odata/Releases")
+        return self.release_key
+
+    def find_process_name(self, release_key):
+        if self.token is None:
+            return None
+
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Bearer " + self.token
+        }
+        params = {
+            "$filter": "Key eq '" + release_key + "'",
+            "api_key": self.api_key
+        }
+        response = requests.get(
+            self.url + "/odata/Releases", headers=headers, params=params)
+        if response.status_code == 200:
+            if response.json()["@odata.count"] > 0:
+                self.process_name = response.json()["value"][0]["Name"]
+            else:
+                self.message = _("Process not found")
         else:
             self.message = _("{} failed").format("/odata/Releases")
         return self.release_key
@@ -100,5 +135,6 @@ class orchestrator:
         if "message" in response.json():
             self.message = response.json()["message"]
         else:
-            self.message = _("{} failed").format("/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs")
+            self.message = _("{} failed").format(
+                "/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs")
         return None
