e834edea80df36d354271cea2aa6de4f646bdbd1
EC2 Default User <miyaichi@gmail.com>
2019-02-14 22:21:11 +0000

    Update

--- handler.py
+++ handler.py
@@ -10,7 +10,7 @@ trans = gettext.translation(
 trans.install()
 
 
-def monitoring(func):
+def handler_wrapper(func):
     def decorate(event, context):
         try:
             log = logging.getLogger()
@@ -27,56 +27,56 @@ def monitoring(func):
     return decorate
 
 
-@monitoring
+@handler_wrapper
 def backlog_handler(event, context):
     import handlers.backlog
     response = handlers.backlog.handler(event, context)
     return response
 
 
-@monitoring
+@handler_wrapper
 def github_handler(event, context):
     import handlers.github
     response = handlers.github.handler(event, context)
     return response
 
 
-@monitoring
+@handler_wrapper
 def google_hangouts_handler(event, context):
     import handlers.google_hangouts
     response = handlers.google_hangouts.handler(event, context)
     return response
 
 
-@monitoring
+@handler_wrapper
 def html_form_handler(event, context):
     import handlers.html_form
     response = handlers.html_form.handler(event, context)
     return response
 
 
-@monitoring
+@handler_wrapper
 def iot_button_handler(event, context):
     import handlers.iot_button
     response = handlers.iot_button.handler(event, context)
     return response
 
 
-@monitoring
+@handler_wrapper
 def orchestrator_handler(event, context):
     import handlers.orchestrator
     response = handlers.orchestrator.handler(event, context)
     return response
 
 
-@monitoring
+@handler_wrapper
 def slack_handler(event, context):
     import handlers.slack
     response = handlers.slack.handler(event, context)
     return response
 
 
-@monitoring
+@handler_wrapper
 def wrike_handler(event, context):
     import handlers.wrike
     response = handlers.wrike.handler(event, context)
